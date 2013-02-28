24920a6acdb9ce985e8172fd517b8503752a0917
Pål Moen Møst <paalmm@tihlde.org>
2013-02-28 15:47:28 +0100

    added test_clean

---
+++ test_clean.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np #   break
+import sys
+import time
+
+cap = cv2.VideoCapture(0)
+fps = 30
+while True:
+    flag, frame = cap.read()
+    #im_rgb = cv2.CreateImage(self.size,  opencv.IPL_DEPTH_8U, 3)
+    #v2.CvtColor(frame, im_rgb, cv2.CV_BGR2RGB)
+    destRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    #cv2.waitKey(30)
+    time.sleep(int(1.0/fps))
+    sys.stdout.write(destRGB.tostring())
+cv2.destroyAllWindows()
