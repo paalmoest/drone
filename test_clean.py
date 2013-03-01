1fe144e3b5dbefbfce549dc9855ab66a862fd705
Pål Moen Møst <paalmm@tihlde.org>
2013-03-01 12:10:08 +0100

    ok

--- test_clean.py
+++ test_clean.py
@@ -5,17 +5,13 @@ import time
 
 cap = cv2.VideoCapture(0)
 fps = 30
-cam_width = 320
-cam_height = 240
 
-cap.set(3, cam_width)
-cap.set(4, cam_height)
 while True:
     flag, frame = cap.read()
     #im_rgb = cv2.CreateImage(self.size,  opencv.IPL_DEPTH_8U, 3)
     #v2.CvtColor(frame, im_rgb, cv2.CV_BGR2RGB)
-    destRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    #cv2.waitKey(30)
-    time.sleep(int(1.0/fps))
+    destRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #scv2.waitKey(10)
+   # time.sleep(int(1.0/fps))
     sys.stdout.write(destRGB.tostring())
 cv2.destroyAllWindows()
