b876ee7b2060da28b2b7b54770b9dbc7c7dfa72e
Pål Moen MØst <paalmm@stud.ntnu.no>
2013-03-18 07:53:02 -0500

    ++

---
+++ fun/timelapse.py
@@ -0,0 +1,15 @@
+import cv2
+import time
+
+
+capture = cv2.VideoCapture(0)
+img_num = 0
+while True:
+	
+	flag, frame = capture.read()
+	destRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	img_num += 1 
+	cv2.imwrite('test_%s.jpeg' % img_num, frame)
+	time.sleep(3)
+
+
