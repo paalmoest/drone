889dd8296fd5db142176bec93602e9754c0ebcbf
Pål Moen Møst <paalmm@tihlde.org>
2013-09-24 13:10:18 +0200

    50hz task

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ class Main:
                         if self.verbose:
                             print self.autopilot.pp_throttle_and_height()
                             #print self.autopilot.pp_receiver_commands() + " marker: " + str(self.marker_spotted)
-                        then = datetime.datetime.now() + datetime.timedelta(seconds=0.1)
+                        then = datetime.datetime.now() + datetime.timedelta(seconds=0.05)
                     if datetime.datetime.now() > altholdtask:
                         self.autopilot.altitude_hold()
                         altholdtask = datetime.datetime.now() + datetime.timedelta(seconds=1)
