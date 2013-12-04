4a2fee4bebd823f45179005db8e8758926872422
Pål Moen Møst <paalmm@tihlde.org>
2013-12-04 12:37:56 +0100

    typo

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ class Main:
                     self.autopilot.send_control_commands()
                 else:
                     self.position_controller.reset_targets()
-                    print self.print_alt_hold()
+                    print self.autopilot.print_alt_hold()
             except KeyboardInterrupt:
                 fps = self.i / (time.time() - fpstime)
                 print 'fps %f ' % fps
