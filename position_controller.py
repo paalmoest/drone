4ec4f9192ae6e8e5c21a3d0b755f65bc2f159d1a
Pål Moen Møst <paalmm@tihlde.org>
2013-12-05 16:31:13 +0100

    control commands

--- position_controller.py
+++ position_controller.py
@@ -66,7 +66,7 @@ class PositionController():
             minimum_thrust=min_t,
         )
         self.heading_pid = PID(
-            P=10,
+            P=2,
             I=0,
             D=0,
             Derivator=0,
