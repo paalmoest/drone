bbc702006c98994e96cb2de5678f0954acc1cda7
Pål Moen Møst <paalmm@tihlde.org>
2013-12-16 16:19:21 +0100

    only kf

--- position_controller.py
+++ position_controller.py
@@ -219,7 +219,8 @@ class PositionController():
         x_position = self.state_estimation_marker.getXposition()
         y_position = self.state_estimation_marker.getYposition()
         x = self.calcualte_xDistance()
-        print 'x: %d y: %d distance: %f x_attitude: %f y_attitude: %f ' % (x_position, y_position, x, self.autopilot.angle_x, self.autopilot.angle_y)
+        correction = self.roll_pid.update(x)
+        print 'x: %d y: %d roll correction: %d distance: %f x_attitude: %f y_attitude: %f ' % (x_position, y_position, correction, self.autopilot.angle_x, self.autopilot.angle_y)
         #roll_correction = self.roll_pid.update(x_distance)
         #pitch_correction = self.pitch_pid.update(y_distance)
         #self.autopilot.roll = self.autopilot.position_hold_pitch + roll_correction
