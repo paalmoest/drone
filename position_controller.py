bcdcb17fb28289db3b781cb816357ab827fdeec9
Pål Moen Møst <paalmm@tihlde.org>
2013-12-16 16:44:02 +0100

    ok

--- position_controller.py
+++ position_controller.py
@@ -213,7 +213,8 @@ class PositionController():
     def positionHold(self):
         if not self.position_hold_init:
             self.position_hold_roll = self.autopilot.roll
-            self.position_hold_pitch = self.autopilot.pitch
+            #self.position_hold_pitch = self.autopilot.pitch
+            print "hello"
             self.roll_pid.setPoint(0.0)
             self.position_hold_init = True
         x_position = self.state_estimation_marker.getXposition()
