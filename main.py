c3ac68cd898e315cbbac4df131f06faa917435df
Pål Moen Møst <paalmm@tihlde.org>
2013-12-18 15:06:35 +0100

    sliter

--- main.py
+++ main.py
@@ -101,6 +101,7 @@ class Main:
                     self.position_controller.reset_targets()
                   #  print 'position x: %f' % self.ukf_position.state[0]
                     print self.autopilot.print_commands()
+                    print self.print_ukf_test()
 
             except KeyboardInterrupt:
                 fps = self.i / (time.time() - fpstime)
