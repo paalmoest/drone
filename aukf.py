c1731361584c6547c712f9fe09449f445bcbf416
Pål Moen Møst <paalmm@tihlde.org>
2013-12-18 16:31:46 +0100

    OK

--- aukf.py
+++ aukf.py
@@ -26,10 +26,10 @@ class UKFPosition():
         if not self.previous_update:
             self.previous_update = time.time()
         self.dt = time.time() - self.previous_update
-        c1 = - 0.1
-        c2 = 0.5
+        c1 = 0.3
+        c2 = 0.3
         x = (state[0] + (state[1] * self.dt)) + noise[0]
-        x_velocity = c1 * ( (np.cos(self.autopilot.heading) * np.sin(self.autopilot.angle_x) * np.cos(self.autopilot.angle_y)) - (np.sin(self.autopilot.heading) * np.sin(self.autopilot.angle_y))) + noise[1]
+        x_velocity = c1 * ((np.cos(self.autopilot.heading) * np.sin(self.autopilot.angle_x) * np.cos(self.autopilot.angle_y)) - (np.sin(self.autopilot.heading) * np.sin(self.autopilot.angle_y))) + noise[1]
         y = (state[2] + (state[3] * self.dt)) + noise[2]
         y_velocity = c2 * (-np.sin(self.autopilot.heading) * np.sin(self.autopilot.angle_x) * np.cos(self.autopilot.angle_y) - np.cos(self.autopilot.heading) * np.sin(self.autopilot.angle_y)) + noise[3]
 
