8cbfa612593447f7cf694b1cd950915b6553c38b
Pål Moen Møst <paalmm@tihlde.org>
2013-12-18 16:25:43 +0100

    ok

--- aukf.py
+++ aukf.py
@@ -54,6 +54,6 @@ class UKFPosition():
             self.kf.filter_update(
                 self.state,
                 self.covariance,
-                [self.autopilot.x_distance_to_marker],
+                [self.autopilot.x_distance_to_marker, self.autopilot.y_distance_to_marker],
             )
         )
