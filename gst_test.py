69fd7d9c6aab88bdaaa91664d8fd55b13392cde3
Pål Moen Møst <paalmm@tihlde.org>
2014-01-02 18:42:26 +0100

    OK

--- gst_test.py
+++ gst_test.py
@@ -9,6 +9,7 @@ import numpy as np
 
 
 class Main:
+
     def __init__(self):
         self.mainloop = gobject.MainLoop()
         self.pipeline = gst.Pipeline("pipeline")
@@ -16,7 +17,7 @@ class Main:
         self.height = 240
         self.videosrc = gst.element_factory_make('v4l2src', 'v4l2src')
         self.vfilter = gst.element_factory_make("capsfilter", "vfilter")
-        #self.vfilter.set_property('caps', gst.caps_from_string(
+        # self.vfilter.set_property('caps', gst.caps_from_string(
         #    'video/x-raw-rgb,format=RGB3, width=%s, height=%s,framerate=30/1' % (str(self.width), str(self.height))))
         self.vfilter.set_property('caps', gst.caps_from_string(
             'image/jpeg, width=%s, height=%s, framerate=30/1' % (str(self.width), str(self.height))))
@@ -33,20 +34,20 @@ class Main:
 
         self.ras_MIN = np.array([150, 80, 80], np.uint8)
         self.ras_MAX = np.array([175, 255, 255], np.uint8)
-    
+
     def onVideoBuffer(self, pad, idata):
-        image = np.asarray(
-            bytearray(idata),
-            dtype=np.uint8,
-        )
-        """
+       # image = np.asarray(
+      #      bytearray(idata),
+      #      dtype=np.uint8,
+      #  )
+   
         image = np.ndarray(
             shape=(self.height, self.width, 3),
             dtype=np.uint8,
             buffer=idata,
         )
-        """
-        frame = image
+
+        #frame = image
         self.i += 1
         print self.i
        # frame = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
