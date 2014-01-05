60a2810a2f3d9f4846733ab132cf0f5ecb99d476
Pål Moen Møst <paalmm@tihlde.org>
2014-01-05 20:29:14 +0100

    OK

--- position_estimator.py
+++ position_estimator.py
@@ -190,7 +190,7 @@ class LinearPosition():
                                             [1, 0, 0, 0, 0, 0],
                                             [0, 1, 0, 0, 0, 0],
                                             ]),
-                transition_offset=u,
+               # transition_offset=u,
             )
         )
         self.previous_update = time.time()
