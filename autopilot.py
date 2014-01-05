daf27cd630d45ab6624570e19ad3707aa0ecb7de
Pål Moen Møst <paalmm@tihlde.org>
2014-01-05 20:06:29 +0100

    my positioN

--- autopilot.py
+++ autopilot.py
@@ -275,6 +275,7 @@ class AutoPilot():
             self.state_estimate.update(np.array([self.altitude_sonar]))
         except:
             pass
+        print self.auto_switch
         self.update_linearKf()
         # self.log()
 
