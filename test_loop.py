dcc42ab7261b1c90a5b8e9ef057a973a5522b6fb
Pål Moen Møst <paalmm@tihlde.org>
2014-01-05 18:29:34 +0100

    threading fucks you up

--- test_loop.py
+++ test_loop.py
@@ -14,11 +14,12 @@ from image_processing import ImageProcessing
 from autopilot import AutoPilot
 #v4l2-ctl --list-formats-ext
 
+
 class Main:
 
     def __init__(self, **kwargs):
 
-    	self.state_estimate = StateEstimationAltitudeSonar()
+        self.state_estimate = StateEstimationAltitudeSonar()
         self.autopilot = AutoPilot(self.state_estimate)
 
         gobject.threads_init()
@@ -35,19 +36,15 @@ class Main:
         self.image_processing = ImageProcessing()
         self.buildRawVideofeed()
         fpstime = time.time()
-        
+
         context = self.mainloop.get_context()
 
         self.i = 0
 
-   
-        
-
         self.pipeline.set_state(gst.STATE_PLAYING)
         while True:
-        	print self.i
-        	context.iteration(True)
-
+            print self.i
+            context.iteration(True)
 
     def onVideoBuffer(self, pad, idata):
 
@@ -61,15 +58,38 @@ class Main:
         return True
 
     def onVideoBufferRaw(self, pad, idata):
+
+        hsv_min = np.array([100, 130, 130], np.uint8)
+        hsv_max = np.array([120, 255, 255], np.uint8)
         image = np.ndarray(
             shape=(self.cam_height, self.cam_width, 3),
             dtype=np.uint8,
             buffer=idata,
         )
         self.i += 1
-        print self.i
+        hsv_img = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        thresh = cv2.inRange(hsv_img, hsv_min, hsv_max)
+        thresh2 = thresh.copy()
+        contours, hierarchy = cv2.findContours(
+            thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        max_area = 0
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area > max_area:
+                max_area = area
+                best_cnt = cnt
+        if max_area > 100:
+            approx = cv2.approxPolyDP(
+                best_cnt, 0.1 * cv2.arcLength(best_cnt, True), True)
+            if len(approx) == 4:
+                M = cv2.moments(best_cnt)
+                cx, cy = int(M['m10'] / M['m00']), int(M['m01'] / M['m00'])
+                x, y, w, h = cv2.boundingRect(best_cnt)
+                areal = w * h
+                rect = cv2.minAreaRect(best_cnt)
         #marker = self.image_processing.recognize_marker(image)
        # self.autopilot.update_marker(marker)
+       	print self.i
         return True
 
     def buildRawVideofeed(self):
