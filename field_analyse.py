95b46d6d6c4570343d0221a02bc7daa9ab04206f
Pål Moen Møst <paalmm@tihlde.org>
2014-01-06 20:15:49 +0100

    aalyse

--- field_analyse.py
+++ field_analyse.py
@@ -2,7 +2,7 @@ import pickle
 import pylab as pl
 import numpy as np
 
-s = 'data/nesheim/test_2'
+s = 'data/nesheim/test_5'
 acceleration = pickle.load(open('%s/acceleration.dump' % s))
 attitude = pickle.load(open('%s/attitude.dump' % s))
 control_commands = pickle.load(open('%s/control_commands.dump' % s))
@@ -264,16 +264,14 @@ def plot_throttle():
     pl.figure()
     t = pl.plot(throttle, color="r")
   #  t_log = pl.plot(throttle_log, color="b")
-plot_altitude()
-plot_marker()
+#plot_altitude()
+#plot_marker()
 plot_pid(pitch_pid, 'pitch')
 plot_pid(roll_pid, 'roll')
 #plot_battery()
-
-#plot_pid(pitch_pid)
-#plot_correction()
 #plot_correction_alt()
 #plot_pid_alt()
+#plot_battery()
 #plot_pid()
 # plot_correction()
 #plot_attitude()
