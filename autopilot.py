4020cb006a9915d681e4dd0149aa052e1aa99438
Pål Moen Møst <paalmm@tihlde.org>
2014-01-07 15:04:31 +0100

    fortegns feil

--- autopilot.py
+++ autopilot.py
@@ -351,8 +351,8 @@ class AutoPilot():
         roll = (self.roll - 1500.0) / 500.0
         pitch = (self.pitch - 1500.0) / 500.0
     #    print 'last: %f now: %f' % (self.last_pitch, pitch)
-        u_pitch = c1 * (self.last_pitch - pitch)
-        u_roll = c1 * (self.last_roll - roll)
+        u_pitch = c1 * (pitch - self.last_pitch) 
+        u_roll = c1 * (roll - self.last_roll)
        # print 'u pitch %f' % u_pitch
         self.last_roll = roll
         self.last_pitch = pitch
