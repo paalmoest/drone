16739a13e9a01371f8f317cb74a7e1ee63b9ce45
Pål Moen Møst <paalmm@tihlde.org>
2014-01-13 15:56:05 +0100

    timer

--- main.py
+++ main.py
@@ -58,8 +58,11 @@ class Main:
                 self.autopilot.read_sensors()
                 if self.autopilot.auto_switch > 1700:
                     self.autopilot.update_linearKf()
-                    self.position_controller.altitudeHoldSonarKalman()
-                    self.position_controller.autoLand()
+                    if self.autopilot.mode > 1500:
+                        self.position_controller.autoLand()
+                    else:
+                        self.position_controller.altitudeHoldSonarKalman()
+                    #self.position_controller.positionHold()
                     self.autopilot.send_control_commands()
                 else:
                     print self.autopilot.print_commands()
