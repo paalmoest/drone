5e9c1402a5735c5a29a6099faee5a66818997ed2
Pål Moen Møst <paalmm@tihlde.org>
2014-01-15 17:09:23 +0100

    OK

--- state_estimation.py
+++ state_estimation.py
@@ -189,6 +189,65 @@ class StateEstimationAltitudeSonar():
     def getVelocity(self):
         return self.state[1]
 
+class StateEstimationAltitudeSonarOffline():
+
+    def __init__(self):
+        self.state = [0, 0]
+        self.covariance = np.eye(2)
+        self.observation_covariance = np.array([
+            [0.5]
+        ])
+        self.transition_covariance = np.array([
+            [0.01, 0],
+            [0, 0.01],
+        ])
+        self.kf = KalmanFilter(
+            transition_covariance=self.transition_covariance,  # H
+            observation_covariance=self.observation_covariance,  # Q
+        )
+        self.previous_update = None
+        self.stack = []
+
+    def removeOutliners(self, observation):
+        if len(self.stack) < 10:
+            self.stack.append(observation)
+        else:
+            avg = np.average(self.stack)
+            if abs(avg - observation) > 0.5:
+                return None
+            else:
+                self.stack.pop(0)
+                self.stack.append(observation)
+                return [observation]
+
+    def update(self, dt, observation):
+        if not self.previous_update:
+            self.previous_update = time.time()
+      #  dt = time.time() - self.previous_update
+        observations = self.removeOutliners(observation)
+        self.state, self.covariance = (
+            self.kf.filter_update(
+                self.state,
+                self.covariance,
+                observations,
+                transition_matrix=np.array([
+                                           [1, dt],
+                                           [0, 1]
+                                           ]),
+                observation_matrix=np.array([
+                                            [1, 0],
+                                            ]),
+            )
+        )
+        self.previous_update = time.time()
+
+    def getAltitude(self):
+        return self.state[0]
+
+    def getVelocity(self):
+        return self.state[1]
+
+
 
 class Attitude():
 
